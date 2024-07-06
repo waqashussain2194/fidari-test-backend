3fded37ff8cb66298c5cf2c668ef2b58c84059d3
Waqas Hussain <waqas.hussain2194@gmail.com>
2024-07-06 18:57:58 -0400

    Readme update

--- src/customer/dto/customer-login.dto.ts
+++ src/customer/dto/customer-login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class CustomerLoginDto {
-  @ApiProperty({ example: 'customer4@example.com' })
+  @ApiProperty({ example: 'customer@example.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'Amperor2598@' })
+  @ApiProperty({ example: 'Hjuh9Y8H9@' })
   @IsString()
   @MinLength(8)
   password: string;
